a1a201ae6de31b81436a79a9eb3ec514b0418ed9
adamchmski <adamchmski@gmail.com>
2023-08-17 15:13:21 -0700

    moved model to a .py file

---
+++ nanoGPT.py
@@ -0,0 +1,101 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+# Hyperparameters 
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300 
+learning_rate = 1e-2
+#device=Metal FIGURE OUT HOW TO RUN ON M1 GPU 
+eval_iters = 200 
+
+
+# Import text data 
+file = open('tinyShakespeare.txt', 'r')
+text = file.read()
+
+# Create string to/from integer mapping
+characters = ''.join(sorted((list(set(text)))))
+vocab_size = len(characters)
+stoi = {c:i for i, c in enumerate(characters)}
+itos = {i:c for i, c in enumerate(characters)}
+
+# Encoding/Decoding functions to/from integer/character
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[n] for n in l])
+
+# Convert text into a encoded torch tensor
+data = torch.tensor(encode(text), dtype=torch.long)
+
+# Create training and testing split of data 
+train_size = int(len(text) * .9)
+train_data = data[:train_size]
+test_data = data[train_size:]
+
+# Function that create batches
+def get_batch(split, batch_size, block_size):
+    data = train_data if split == 'train' else test_data
+    ix = torch.randint(len(data) - block_size, (batch_size, ))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y 
+
+# Bigram model class 
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        
+    def forward(self, inputs, targets=None):
+        logits = self.token_embedding_table(inputs)
+
+        if targets == None:
+            loss = None
+        else: 
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:,-1,:]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+            
+        return idx 
+
+# Create the BigramLanguageModel and optimizer
+model = BigramLanguageModel(vocab_size)
+optimizer = torch.optim.AdamW(model.parameters())
+
+# Train model 
+for step in range(10000):
+    # Get a batch to train on
+    x, y = get_batch('train', batch_size, block_size)
+    
+    # Reset the parameter gradients 
+    optimizer.zero_grad(set_to_none=True)
+
+    # Forward Pass 
+    logits, loss = model.forward(x, y)
+    if (step % 1000) == 0:
+        print(f"At step {step} loss = {loss.item()}")
+
+    # Backward Pass 
+    loss.backward() 
+    optimizer.step()
+    
+
+# Generate text from the model
+print("Generating text...\n")
+start_idx = torch.zeros((1,1), dtype=torch.long)
+generation = model.generate(idx=start_idx, max_new_tokens=500)[0]
+decoded_generation = decode(generation.tolist())
+print(decoded_generation)
